9c61e7fe49341c1ce76763cb11d0bf76af7b7b94
bhatiashubham <128917348+bhatiashubham@users.noreply.github.com>
2023-07-15 10:39:21 +0530

    Add files via upload

---
+++ app/db.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+exports.connectMongoose =()=>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect('mongodb+srv://himadrinayak:12345@cluster0.h7n86ah.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
+    .catch((e)=>console.log("Not Connect Mongodb", e));
+}
